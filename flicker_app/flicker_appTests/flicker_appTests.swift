09814498a0eef085d51078be5921247152023cf3
Gulshid Zada <gulshidzada4@gmail.com>
2026-08-14 11:42:20 +0500

    New project strtaed

---
+++ flicker_app/flicker_appTests/flicker_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  flicker_appTests.swift
+//  flicker_appTests
+//
+//  Created by Gulshid Zada on 14/08/2026.
+//
+
+import Testing
+@testable import flicker_app
+
+struct flicker_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
